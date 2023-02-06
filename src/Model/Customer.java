7ec195b292c20f0c1d993981d3b0c1e4c5470683
ForumKaria <karia.f@northeastern.edu>
2023-02-06 15:40:47 -0500

    Update

---
+++ src/Model/Customer.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author forumkaria
+ */
+public
+        class Customer {
+    
+    String fullName;
+        int custId;
+
+    public
+    String getFullName() {
+        return fullName;
+    }
+
+    public
+    void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public
+    int getCustId() {
+        return custId;
+    }
+
+    public
+    void setCustId(int custId) {
+        this.custId = custId;
+    }
+        
+}
+
